b86b58034087f20bce4d2e397f09ee6845d4c30d
Cara Zellner <carajeanine@gmail.com>
2020-10-14 18:50:33 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,22 +1,22 @@
 # CashRegister
 #   ::new
-#     sets an instance variable @total on initialization to zero (FAILED - 1)
-#     optionally takes an employee discount on initialization (FAILED - 2)
+#     sets an instance variable @total on initialization to zero
+#     optionally takes an employee discount on initialization 
 #   #total
-#     returns the current total (FAILED - 3)
+#     returns the current total 
 #   #add_item
-#     accepts a title and a price and increases the total (FAILED - 4)
-#     also accepts an optional quantity (FAILED - 5)
-#     doesn't forget about the previous total (FAILED - 6)
+#     accepts a title and a price and increases the total 
+#     also accepts an optional quantity 
+#     doesn't forget about the previous total 
 #   #apply_discount
 #     the cash register was initialized with an employee discount
-#       applies the discount to the total price (FAILED - 7)
-#       returns success message with updated total (FAILED - 8)
-#       reduces the total (FAILED - 9)
+#       applies the discount to the total price 
+#       returns success message with updated total 
+#       reduces the total 
 #     the cash register was not initialized with an employee discount
-#       returns a string error message that there is no discount to apply (FAILED- 10)
+#       returns a string error message that there is no discount to apply 
 #   #items
-#     returns an array containing all items that have been added (FAILED - 11)
+#     returns an array containing all items that have been added 
 #   #void_last_transaction
-#     subtracts the last item from the total (FAILED - 12)
-#     returns the total to 0.0 if all items have been removed (FAILED - 13)
+#     subtracts the last item from the total 
+#     returns the total to 0.0 if all items have been removed 
